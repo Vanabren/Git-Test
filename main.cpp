3e86e0bdbaaf6a170cb610d1d45da7f46aee83ec
Vanabren <vanabren@ut.utm.edu>
2022-08-26 12:34:10 -0500

    Simple Hello World! Program

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Vance Brenderabrandis
+ * @date 2022-08-26
+ * @brief Simple Hello World program
+ * 
+ * The program outputs the sentence "Hello World!" to the console.
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
